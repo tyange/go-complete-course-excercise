3aedae283c2f96c74092eff7b67bcf3f5daf7a4e
tyange <usun16@gmail.com>
2022-12-04 21:57:41 +0900

    Initial commit

---
+++ excercise.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Tyange"
+	var lastName = "Yu"
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+	currentYear := 2022
+	var birthYear int
+	birthYear = 1990
+
+	age := currentYear - birthYear
+	fmt.Println(age)
+
+	currentYear = currentYear + 1
+	age = currentYear - birthYear
+	fmt.Println(age)
+}
